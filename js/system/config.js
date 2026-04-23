6795ab24e373fb119a05b94f2bc6a9728b29d379
muttinon21140 <muttinon21140@gmail.com>
2026-04-23 15:48:02 +0700

    Add files via upload

--- js/system/config.js
+++ js/system/config.js
@@ -1,6 +1,7 @@
 // config.js
 
-const liffId = "2007981677-Z8m3omk4";
+// const liffId = "2007981677-Z8m3omk4";
+const liffId = "2009878626-FROHiJMY";
 
 const NETLIFY_CHECKUSER_URL = "https://petpettest.netlify.app/.netlify/functions/checkUser";
 const NETLIFY_REGISTERUSER_URL = "https://petpettest.netlify.app/.netlify/functions/registerUser";
